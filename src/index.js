c6ec894ec49bd966e7f71b1b8bde2356fdbd5210
vishnuforcode <vishnuforgrow@gmail.com>
2026-02-03 19:31:16 +0530

    tweeks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter , createRoutesFromElements , Route, RouterProvider} from 'react-router-dom'
 import Home from './pages/Home';
